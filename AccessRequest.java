66c4a143d97c4df3ad69a0ae89e64597830b8be6
zzxxlty <168614983+zzxxlty@users.noreply.github.com>
2024-05-06 08:55:11 -0700

    Create AccessRequest.java

---
+++ AccessRequest.java
@@ -0,0 +1,10 @@
+// AccessRequest.java
+import java.util.List;
+
+public class AccessRequest {
+    private int userId;
+    private List<String> endpoints;
+    private String role;
+
+    // Getters and setters
+}
